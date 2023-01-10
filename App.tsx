e5c4a49976c5e974d74e1f41a3da5dc4440a0d74
John Everton <johneverton44@gmail.com>
2023-01-10 08:26:17 -0300

    config tab navigation

--- App.tsx
+++ App.tsx
@@ -10,9 +10,10 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes'
+
 
 
 export default function App() {
@@ -29,7 +30,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-        <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
